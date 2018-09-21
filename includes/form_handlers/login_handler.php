8286e68fc79da0bc2c68db496fb16a848225202f
xFuture603 <daniel.xfuture@googlemail.com>
2018-09-21 10:10:20 +0200

    added bcrypt now for my pw hash - works fine!

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -1,32 +1,11 @@
 <?php
-    /*if (isset($_POST['login_button'])) {
-        $email = filter_var($_POST['log_email'], FILTER_SANITIZE_EMAIL);
-        $_SESSION['log_email'] = $email;
-        $password= $_POST['log_password'];
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
- 
-        $check_database_query = mysqli_query($conn,"SELECT * FROM users WHERE email='$email' AND password='$hashedPassword'");
-        $check_login_query = mysqli_num_rows($check_database_query);
- 
-        if ($check_login_query == 1) {
-            $row = mysqli_fetch_array($check_database_query);
-            $username = $row['username'];
- 
-            $_SESSION['username']=$username;
-            header("Location: index.php");
-            exit();
-        }
-        else{
-            array_push($error_array, "Email Or Password was incorrect<br>");
-        }
-    }*/
-
-if(isset($_POST['login_button'])) {
+/*if(isset($_POST['login_button'])) {
 
   $email = filter_var($_POST['log_email'], FILTER_SANITIZE_EMAIL); //sanitize email
 
-  $_SESSION['log_email'] = $email; //Store the email into session variable
-  $password = md5($_POST['log_password']); //Get password
+  //$_SESSION['log_email'] = $email; //Store the email into session variable
+  //$password = md5($_POST['log_password']); //Get password
+  $password = $_POST["log_password"];
 
   $check_database_query = mysqli_query($conn, "SELECT * FROM users WHERE email='$email' AND password='$password'");
   $check_login_query = mysqli_num_rows($check_database_query);
@@ -39,5 +18,29 @@ if(isset($_POST['login_button'])) {
     header("Location: index.php");
     exit();
   }
+}*/
+
+  
+if (isset($_POST["login_button"])){      
+// ensure email is in sanitized (valid) format and assign to var      
+$email = filter_var($_POST["log_email"], FILTER_SANITIZE_EMAIL);      // create an email session      
+$_SESSION["log_email"] = $email;       
+// check the password against the password_hash      
+$password = $_POST["log_password"];      
+// get hash     
+$check_database_query = mysqli_query($conn, "SELECT password, username, email from users WHERE email='$email'");      
+$row = mysqli_fetch_array($check_database_query);      
+// get the value of the password column      
+$hash = $row["password"];      
+// verifies the input password against the hashed password      
+$password_verify = password_verify($password, $hash);      
+// if a record is found, grab the data from the username column      
+if ($password_verify == 1){         $username = $row["username"];         
+// create a new session with username         
+$_SESSION["username"] = $username;         
+// redirect user to index page         
+header("Location: index.php");         
+exit();      
+}   
 }
 ?>

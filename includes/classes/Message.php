4c24cb720bd9cfeec4452dde81d73e6a56ded998
xFuture603 <daniel.xfuture@googlemail.com>
2018-10-02 09:06:46 +0200

    wrote a function to get the MostRecentUser for my messages

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -12,6 +12,26 @@ class Message
     $this->user_obj = new User($conn, $user);
     }
 
+    public function getMostRecentUser() {
+      $userLoggedIn = $this->user_obj->getUsername();
+
+      $query = mysqli_query($this->conn, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' 
+      ORDER BY id DESC LIMIT 1");
+
+      if(mysqli_num_rows($query) == 0)
+        return false;
+
+      $row = mysqli_fetch_array($query);
+      $user_to = $row['user_to'];
+      $user_from = $row['user_from'];
+
+      if($user_to != $userLoggedIn)
+        return $user_to;
+      else
+        return $user_from;
+
+    }
+
     
   }
 ?>
